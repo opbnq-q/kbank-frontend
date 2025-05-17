3ba6a22070aae73412a2c00e69bff384f43b940a
opbnq-q <cko.vladislav@gmail.com>
2025-05-17 16:52:13 +0700

    prettify lang switcher

--- features/lang-switcher/ui/LangSwitcher.vue
+++ features/lang-switcher/ui/LangSwitcher.vue
@@ -14,7 +14,7 @@ const handleClick = (l: Locale) => {
 
 <template>
     <div class="relative w-[100px] flex items-center justify-center">
-        <SharedBaseButton @click="isOpened = true">{{ locale }}</SharedBaseButton>
+        <SharedBaseButton :class="['w-full', {'scale-50': isOpened}]" @click="isOpened = true">{{ locales.find(el => el.code == locale)?.name }}</SharedBaseButton>
         <section
             :class="['absolute top-0 left-0 z-50 flex w-full flex-col border-[1px] border-primary-border p-2 rounded-xl items-center gap-2 transition bg-secondary-bg', { 'invisible scale-75 opacity-0': !isOpened }]">
             <SharedBaseButton class="w-20" v-for="l in locales" @click="handleClick(l.code)">
