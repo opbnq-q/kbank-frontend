d46a3e03e395fe57be70c82181652acb8145dd46
opbnq-q <cko.vladislav@gmail.com>
2025-06-03 23:51:53 +0700

    prefix sums line

--- features/debts-chart/ui/DebtsChart.vue
+++ features/debts-chart/ui/DebtsChart.vue
@@ -1,5 +1,5 @@
 <template>
-  <Line :data="data" :options="options" />
+    <Line :data="data" :options="options" />
 </template>
 
 <script lang="ts" setup>
@@ -7,18 +7,31 @@ import { Line } from 'vue-chartjs'
 import type { ChartData, ChartOptions } from 'chart.js'
 
 const props = defineProps<{
-  debt: Debt
-  completeRequestsTape: ReturnType<typeof useCompleteRequestsTapeStore>
+    debt: Debt
 }>()
 
+const completeRequestsTape = useCompleteRequestsTapeStore()
+
+const prices = computed(() =>
+    completeRequestsTape.acceptedCompleteRequests(props.debt.id)?.map(
+        el => el.price * props.debt.currency.standardUnits
+    ) ?? []
+)
+
+const prefixSums = computed(() => {
+    const arr = prices.value
+    let sum = 0
+    return arr.map(v => sum += v)
+})
+
 const data = computed<ChartData<'line'>>(() => ({
-  labels: props.completeRequestsTape.acceptedCompleteRequests(props.debt.id)?.map(el => el.title) ?? [],
-  datasets: [{
-    data: props.completeRequestsTape.acceptedCompleteRequests(props.debt.id)?.map(el => el.price * props.debt.currency.standardUnits) ?? [],
-    borderColor: '#2979FF',
-    backgroundColor: '#2979FF20',
-    fill: true,
-  }]
+    labels: completeRequestsTape.acceptedCompleteRequests(props.debt.id)?.map(el => el.title) ?? [],
+    datasets: [{
+        data: prefixSums.value,
+        borderColor: '#2979FF',
+        backgroundColor: '#2979FF20',
+        fill: true,
+    }]
 }))
 
 const options: ChartOptions<'line'> = {
