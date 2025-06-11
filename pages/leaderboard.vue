c21400265a328cc604f93d6d55aa7db3967d6d8a
opbnq-q <cko.vladislav@gmail.com>
2025-06-11 21:14:07 +0700

    add animation to leaderboard

--- pages/leaderboard.vue
+++ pages/leaderboard.vue
@@ -1,9 +1,9 @@
 <template>
     <ClientOnly>
         <SharedHeaderText>Top #50</SharedHeaderText>
-        <div class="flex flex-col h-full min-h-screen justify-between">
+        <div class="flex flex-col h-full min-h-[90dvh] justify-between">
             <div class="flex flex-col gap-4 max-sm:gap-8">
-                <div v-for="(user, index) in users" class="flex gap-4 items-center h-28 max-sm:flex-col max-sm:h-auto">
+                <motion.div layout :key="user.id" v-for="(user, index) in users" class="flex gap-4 items-center h-28 max-sm:flex-col max-sm:h-auto">
                     <SharedInfoCard class="sm:aspect-square h-full flex items-center justify-center max-sm:w-full">
                         <p class="sm:text-3xl sm:opacity-80">
                             {{ index + 1 }}
@@ -12,7 +12,7 @@
                     <LazyEntityUserCard class="h-full w-full" :balance="user.balance" :id="user.id"
                         :name="`${user.firstName} ${user.lastName}`">
                     </LazyEntityUserCard>
-                </div>
+                </motion.div>
             </div>
             <footer class="flex items-center py-10 justify-center cursor-pointer">
                 <SharedLogo @click="navigateTo('/')" width="150"></SharedLogo>
@@ -22,7 +22,8 @@
 </template>
 
 <script setup lang="ts">
-import { io } from 'socket.io-client';
+import { motion } from 'motion-v';
+import { io, Socket } from 'socket.io-client';
 
 interface LeaderboardUser {
     balance: number
@@ -35,14 +36,18 @@ const users = useState<LeaderboardUser[]>('leaderboard-users', () => [])
 
 const { public: { wsBase } } = useRuntimeConfig()
 
-
+let ws: Socket | null = null
 onMounted(() => {
-    const ws = io(wsBase, {
+    ws = io(wsBase, {
         autoConnect: false
     })
     ws.emit('leaderboard', console.log)
     ws.on('leaderboard-update', (response: LeaderboardUser[]) => users.value = response)
     ws.connect()
+})
+
+onBeforeUnmount(() => {
+    ws && ws.disconnect()
 })
 
 </script>
