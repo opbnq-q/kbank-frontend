4647c60c7fb4e417286e4c9a8dbf4d8d3e525214
opbnq-q <cko.vladislav@gmail.com>
2025-06-11 20:22:01 +0700

    add animations to notificaions and empty-img

--- features/notifications/ui/Notifications.vue
+++ features/notifications/ui/Notifications.vue
@@ -1,17 +1,20 @@
 <template>
     <ClientOnly>
-        <div class="flex flex-col items-end gap-2">
-            <EntityToast class="relative" @remove="notificationsStore.remove(n)"
-                v-for="n in notificationsStore.notifications">
-                <h1>{{ n.message }}</h1>
-                <SharedInternalLink :to="n.link" v-if="n.link">{{ t('go') }}
-                </SharedInternalLink>
-            </EntityToast>
-        </div>
+        <ul class="flex flex-col items-end gap-2">
+            <motion.li layout :key="index" v-for="(n, index) in notificationsStore.notifications">
+                <EntityToast class="relative" @remove="notificationsStore.remove(n)">
+                    <h1>{{ n.message }}</h1>
+                    <SharedInternalLink :to="n.link" v-if="n.link">{{ t('go') }}
+                    </SharedInternalLink>
+                </EntityToast>
+            </motion.li>
+        </ul>
     </ClientOnly>
 </template>
 
 <script setup lang="ts">
+import { motion } from 'motion-v';
+
 const notificationsStore = useNotificationsStore()
 const { t } = useI18n()
 
--- shared/components/pics/empty-img/EmptyImg.vue
+++ shared/components/pics/empty-img/EmptyImg.vue
@@ -1,15 +1,50 @@
 <template>
   <div class="flex flex-col items-center">
-    <h1 class="text-5xl">🐔</h1>
-    <h1 class="text-sm font-bold">{{ t('nothing') }}</h1>
+    <div class="h-16 flex items-center justify-center aspect-square">
+      <AnimatePresence mode="wait">
+        <motion.h1 :exit="{ opacity: 0, scale: 0 }" :initial="{ opacity: 0, scale: 0 }"
+          :animate="{ opacity: 1, scale: 1 }" v-if="isToggled" class="text-5xl">🐔</motion.h1>
+        <motion.div v-else class="grid grid-cols-2 gap-2">
+          <motion.h2 v-for="hen in hens" :key="hen" layout>{{ hen }}</motion.h2>
+        </motion.div>
+      </AnimatePresence>
+    </div>
+    <h1 class="text-sm font-bold flex">
+      <motion.div layout :initial="{ opacity: 0, y: 50 }" :animate="{ opacity: 1, y: 0 }" :transition="{
+        duration: 0.3,
+        delay: index * 0.1,
+      }" v-for="(letter, index) in t('nothing')" :key="index + locale">
+        <p v-if="letter != ' '">{{ letter }}</p>
+        <p v-else class="w-2 h-2"></p>
+      </motion.div>
+    </h1>
   </div>
 </template>
 
 <script lang="ts" setup>
-const { t } = useI18n()
+import { AnimatePresence, motion } from 'motion-v';
 
+const { t, locale } = useI18n()
+
+const [isToggled, toggle] = useToggle()
+let intervals: ReturnType<typeof setInterval>[] = []
+const hens = ref(['🐔', '🐓', '🐣', '🐤'])
+const shuffle = (array: string[]) => {
+  return array.sort(() => Math.random() - 0.4)
+}
+
+onMounted(() => {
+  intervals.push(setInterval(() => {
+    toggle()
+  }, 3000))
+  intervals.push(setInterval(() => {
+    hens.value = shuffle(hens.value)
+  }, 1000))
+})
+
+onBeforeUnmount(() => {
+  intervals.forEach(clearInterval)
+})
 </script>
 
-<style>
-
-</style>
+<style></style>
