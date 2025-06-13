8bf281ae3969e646c45d012fa47b6e139aebc591
opbnq-q <cko.vladislav@gmail.com>
2025-06-13 14:43:01 +0700

    change nav layout

--- widgets/nav/ui/Nav.vue
+++ widgets/nav/ui/Nav.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="w-full h-18 flex items-center justify-between">
         <SharedLogo width="100" @click="navigateTo('/')" class="cursor-pointer max-sm:hidden"></SharedLogo>
-        <div class="flex items-center gap-4 max-sm:justify-between max-sm:w-full">
+        <div class="flex items-center gap-6 max-sm:justify-between max-sm:w-full">
             <div class="w-20">
                 <SharedBaseButton back square v-if="$route.fullPath.length > 1" />
             </div>
